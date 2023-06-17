c9ff30bca7240309f53913294dbf4bbe5dd3c0e3
Kevin DuBartell <kdubartell@gmail.com>
2023-06-16 23:02:12 -0600

    Added logging and dynamic config params to pipeline

--- procedures/mlb_pipeline_proc.sql
+++ procedures/mlb_pipeline_proc.sql
@@ -13,3 +13,22 @@ as
 			set @v_dyn_sql_2 = 'insert  into pitch_fact select * from new_pitch_vw where game_key not in (select distinct game_key from new_pitch_vw)'
 			execute sp_executesql  @v_dyn_sql_2
 		end 
+	begin 
+		declare @max_date date
+		declare cursor_config cursor for 
+			select max(game_date) from config_param
+		open cursor_config 
+		fetch next from cursor_config into @max_date
+
+		while @@FETCH_STATUS = 0
+
+		begin 
+		
+			update config_param set run_status = 'success' where game_date = @max_date
+			update config_param set next_game_date = dateadd(day,1,@max_date) where game_date = @max_date
+			insert into config_param values(concat('report run at ',cast(datetrunc(day,CURRENT_TIMESTAMP) as nvarchar(max))), dateadd(day,1,@max_date), null, null)
+			fetch next from cursor_config into @max_date
+		end
+		close cursor_config
+		deallocate cursor_config
+	end
--- tables/create config_param.sql
+++ tables/create config_param.sql
@@ -1,8 +1,8 @@
 create table config_param (
-prev_run_status varchar(30),
+prev_run_status nvarchar(max),
 game_date date,
 next_game_date date,
-run_status varchar(30)
+run_status nvarchar(max)
 );
 
 insert into config_param values ('initial run' , cast('2023-04-04' as date), null, null);
